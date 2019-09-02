ec2d488fcd46d6fd94abf544e336aaa7e6ed9c0b
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2019-09-02 17:19:57 +0900

    :zap: Enabled knobs to change city, unit, forecast and so on

--- weatherinfo/weatherinfo/weatherinfo.py
+++ weatherinfo/weatherinfo/weatherinfo.py
@@ -1,8 +1,11 @@
 import json
 import traceback
+from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
+from typing import Union
 
+import iterm2
 import requests
 
 
@@ -113,27 +116,25 @@ class WeatherInfo:
         print(f'3hrs Weather: {self.get_char(self.w3i)}  {self.w3} - {self.w3d}')
         print(f'3hrs Temperature: \uED0E  {self.t3}')
 
-    def iterm_format(self) -> str:
-        sep = '\uEB5E'
-        th_char = '\uED0E'
+    def iterm_format(self, use_icon=True, show_forecast=True) -> str:
+        sep = '\uEB5E' if use_icon else '▶'
+        th_char = '\uED0E ' if use_icon else ''
         unit = ''
-        wc_char = self.get_char(self.wci)
-        w3_char = self.get_char(self.w3i)
-        text = f'{wc_char} {self.wc} {th_char} {self.tcs}{unit}'\
-               f' {sep} '\
-               f'{w3_char} {self.w3} {th_char} {self.t3s}{unit}'
+        wc_char = f'{self.get_char(self.wci)} ' if use_icon else ''
+        w3_char = f'{self.get_char(self.w3i)} ' if use_icon else ''
+
+        weather_now = f'{wc_char}{self.wc} {th_char}{self.tcs}{unit}'
+        forecast = f' {sep} {w3_char}{self.w3} {th_char}{self.t3s}{unit}'
+
+        if show_forecast:
+            text = weather_now + forecast
+        else:
+            text = weather_now
 
         return text
 
 
-CONFIG = json.load((Path(__file__).parent / 'config.json').open())
-APIKEY = CONFIG['OpenWeatherAPIKey']
-LOGFILE_PATH = Path('/tmp/iterm-weatherinfo.log')
-w = WeatherInfo()
-w.APIKEY = APIKEY
-
-
-def is_right_time():
+def is_refresh_time():
     now = datetime.now()
     return now.minute % 10 == 0
 
@@ -143,37 +144,77 @@ def have_log():
 
 
 def read_log():
-    return LOGFILE_PATH.open().readline()
+    return json.load(LOGFILE_PATH.open())
 
 
-def write_log(text):
-    with LOGFILE_PATH.open('w') as f:
-        f.write(text)
+def write_log(city: str, use_icon: bool, units: str, show_forecast: bool, weather_info: str):
+    log_data = {
+        'city': city,
+        'use_icon': use_icon,
+        'units': units,
+        'show_forecast': show_forecast,
+        'weather_info': weather_info
+    }
+    json.dump(log_data, LOGFILE_PATH.open('w'), ensure_ascii=False)
 
 
-import iterm2
+@dataclass
+class KnobOption:
+    name: str
+    v: Union[str, bool]
+
+
+CONFIG = json.load((Path(__file__).parent / 'config.json').open())
+APIKEY = CONFIG['OpenWeatherAPIKey']
+LOGFILE_PATH = Path('/tmp/iterm-weatherinfo.log')
+w = WeatherInfo()
+w.APIKEY = APIKEY
 
 
 async def main(connection):
+    knob_city = KnobOption('city', 'Shibuya')
+    knob_use_icon = KnobOption('use_icon', True)
+    knob_use_imperial = KnobOption('use_imperial', False)
+    knob_show_forecast = KnobOption('show_forecast', True)
+    knobs = [
+        iterm2.StringKnob('City', knob_city.v, knob_city.v, knob_city.name),
+        iterm2.CheckboxKnob('Use icon', knob_use_icon.v, knob_use_icon.name),
+        iterm2.CheckboxKnob('Show forecast', knob_show_forecast.v, knob_show_forecast.name),
+        iterm2.CheckboxKnob('Use imperial units', knob_use_imperial.v, knob_use_imperial.name),
+    ]
+
     component = iterm2.StatusBarComponent(
         short_description='Weather Info',
         detailed_description='A component that will tell you the current weather and the forecast.',
-        knobs=[],
+        knobs=knobs,
         exemplar=' Clouds  27.1   Clear  27.1',
         update_cadence=60,
         identifier='peinan.weather'
     )
 
+    def is_true_knob(knobs, knob_option: KnobOption):
+        return bool(knob_option.name in knobs and knobs[knob_option.name])
+
     @iterm2.StatusBarRPC
     async def weather_info(knobs):
-        if not is_right_time() and have_log():
-            return read_log()
+        w.CITYNAME = knobs[knob_city.name] if is_true_knob(knobs, knob_city) else 'Shibuya'
+        w.UNITS = 'imperial' if is_true_knob(knobs, knob_use_imperial) else 'metric'
+        use_icon = knobs[knob_use_icon.name] if is_true_knob(knobs, knob_use_icon) else False
+        show_forecast = knobs[knob_show_forecast.name] if is_true_knob(knobs, knob_show_forecast) else False
 
-        print('[application info] Fetching weather')
+        # print(w.CITYNAME, w.UNITS, use_icon, show_forecast)  # for debug
+
+        if have_log():
+            log_data = read_log()
+            if not is_refresh_time() and (w.CITYNAME == log_data['city'] and w.UNITS == log_data['units'] and
+               use_icon == log_data['use_icon'] and show_forecast == log_data['show_forecast']):
+                return log_data['weather_info']
+
+        print(f'[application info] Fetching weather: {knobs}')
         w.fetch_weather()
         w.parse_weather()
-        weather_info = w.iterm_format()
-        write_log(weather_info)
+        weather_info = w.iterm_format(use_icon=use_icon, show_forecast=show_forecast)
+        write_log(w.CITYNAME, use_icon, w.UNITS, show_forecast, weather_info)
 
         return weather_info
 
